d7e48893dfa80c28d71b8ec610bbcb9a51eb5a3b
gyu <qkr9748200@gmail.com>
2024-12-09 11:43:02 +0900

    버튼 기능 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Chat from "../components/Chat";
